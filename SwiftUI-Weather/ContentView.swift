2373e5222e97414f0615ec65aded5bc11d5ca262
Alexander Ostrovsky <thealeos@gmail.com>
2021-02-05 06:50:57 +0300

    Challenge: add a forecast stack

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -30,6 +30,74 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
                 }
+                HStack(spacing: 20) {
+                    VStack {
+                        Text("TUE")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 40)
+                        Text("74°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                    VStack {
+                        Text("WED")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                        Image(systemName: "sun.max.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("70°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                    VStack {
+                        Text("THU")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                        Image(systemName: "wind")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("66°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                    VStack {
+                        Text("FRI")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                        Image(systemName: "sunset.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("60°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                    VStack {
+                        Text("SAT")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                        Image(systemName: "moon.stars.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("55°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                }
+                .frame(height: 200)
                 Spacer()
             }
         }
