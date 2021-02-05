24d16cb3961d1f26f2de248a282673b70a23f96e
Alexander Ostrovsky <thealeos@gmail.com>
2021-02-05 07:48:36 +0300

    Extract Background View

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
             VStack {
                 Text("Cupertino, CA")
                     .font(.system(size: 32, weight: .medium))
@@ -65,6 +62,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct WeatherDayView: View {
+    
     var dayOfWeek: String
     var imageName: String
     var temperature: Int
@@ -83,5 +81,18 @@ struct WeatherDayView: View {
                 .font(.system(size: 28, weight: .medium))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
     }
 }
